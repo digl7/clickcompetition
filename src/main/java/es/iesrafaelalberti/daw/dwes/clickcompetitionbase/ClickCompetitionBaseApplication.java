c6acc69a8a282b06fe3771dc99f2ff498ed10cbd
Digl7 <dgl1997@gmail.com>
2021-02-22 14:39:54 +0100

    sistema login pocho y el css pocho también.

--- src/main/java/es/iesrafaelalberti/daw/dwes/clickcompetitionbase/ClickCompetitionBaseApplication.java
+++ src/main/java/es/iesrafaelalberti/daw/dwes/clickcompetitionbase/ClickCompetitionBaseApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -22,15 +24,36 @@ public class ClickCompetitionBaseApplication {
     class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.cors().disable()
-                    .csrf().disable()
+            http.csrf().disable()
                     .authorizeRequests()
-                    .antMatchers("/").permitAll();
+                    .antMatchers("/*").hasAnyRole("player")
+                    .antMatchers("/demo1.html").hasRole("player")
+                    .and().formLogin().defaultSuccessUrl("/index.html");
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("españolito1").password("españolito1").roles("player")
+                    .and()
+                    .withUser("facha").password("facha").roles("player")
+                    .and()
+                    .withUser("podemitas").password("podemitas").roles("player")
+                    .and()
+                    .withUser("votante").password("votante").roles("player")
+                    .and()
+                    .withUser("maccarrani").password("maccarrani").roles("player")
+                    .and()
+                    .withUser("spagetti").password("spagetti").roles("player")
+                    .and()
+                    .withUser("corona").password("corona").roles("player")
+                    .and()
+                    .withUser("virus").password("virus").roles("nada");
         }
 
         @Bean
         public PasswordEncoder getPasswordEncoder() {
-            return new BCryptPasswordEncoder();
+            return NoOpPasswordEncoder.getInstance();
         }
 
     }
